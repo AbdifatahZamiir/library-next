7232bd6a8f4987ea8573ee022197953f76901cf8
Abdifatah <abdifatahzammiir@gmail.com>
2020-07-26 09:51:04 +0300

    add new files

--- pages/index.js
+++ pages/index.js
@@ -2,9 +2,9 @@ import Head from "next/head";
 import Layout from "../components/layout";
 
 export default function Home() {
-  return (
-    <Layout>
-      <h1> HELLO WORLD</h1>
-    </Layout>
-  );
+	return (
+		<Layout>
+			<h1> HELLO WORLD</h1>
+		</Layout>
+	);
 }
