3d7c8fa7470bc54226c76d36a2f83ea9ad46f11a
Abdifatah <abdifatahzammiir@gmail.com>
2020-07-26 12:14:16 +0300

    add awesome section

---
+++ components/awesome.js
@@ -0,0 +1,91 @@
+export default function Awesome() {
+	return (
+		<section className="feature_area section_gap_top">
+			<div className="container">
+				<div className="row justify-content-center">
+					<div className="col-lg-5">
+						<div className="main_title">
+							<h2 className="mb-3">Awesome Feature</h2>
+							<p>
+								Replenish man have thing gathering lights
+								yielding shall you
+							</p>
+						</div>
+					</div>
+				</div>
+				<div className="row">
+					<div className="col-lg-4 col-md-6">
+						<div className="single_feature">
+							<div className="icon">
+								<span className="flaticon-student">
+									<img
+										className="icons"
+										src="/images/icons/bookshelf.svg"
+										alt="bookshelf"
+									/>
+								</span>
+							</div>
+							<div className="desc">
+								<h4 className="mt-3 mb-2">
+									Scholarship Facility
+								</h4>
+								<p>
+									One make creepeth, man bearing theira
+									firmament won't great heaven
+								</p>
+							</div>
+						</div>
+					</div>
+
+					<div className="col-lg-4 col-md-6">
+						<div className="single_feature">
+							<div className="icon">
+								<span className="flaticon-book">
+									{" "}
+									<img
+										className="icons"
+										src="/images/icons/reading-book.svg"
+										alt="reading books"
+									/>
+								</span>
+							</div>
+							<div className="desc">
+								<h4 className="mt-3 mb-2">
+									Sell Online Course
+								</h4>
+								<p>
+									One make creepeth, man bearing theira
+									firmament won't great heaven
+								</p>
+							</div>
+						</div>
+					</div>
+
+					<div className="col-lg-4 col-md-6">
+						<div className="single_feature">
+							<div className="icon">
+								<span className="flaticon-earth">
+									{" "}
+									<img
+										className="icons"
+										src="/images/icons/library.svg"
+										alt="library"
+									/>
+								</span>
+							</div>
+							<div className="desc">
+								<h4 className="mt-3 mb-2">
+									Global Certification
+								</h4>
+								<p>
+									One make creepeth, man bearing theira
+									firmament won't great heaven
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+	);
+}
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,11 @@
 import Head from "next/head";
 import Layout from "../components/layout";
+import Awesome from "../components/awesome";
 
 export default function Home() {
-	return <Layout></Layout>;
+	return (
+		<Layout>
+			<Awesome />
+		</Layout>
+	);
 }
