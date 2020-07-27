713548ffe3231594ddd89c5cf10cf29a7c4483bf
Abdifatah <abdifatahzammiir@gmail.com>
2020-07-27 12:28:46 +0300

    add book page

--- pages/index.js
+++ pages/index.js
@@ -6,12 +6,13 @@ import Popular from "../components/popular";
 import Registration from "../components/registration";
 import Team from "../components/team";
 import Events from "../components/events";
-
+import Books from "../components/books";
 export default function Home() {
 	return (
 		<Layout>
 			<Slider />
 			<Awesome />
+			<Books />
 			<Registration />
 			<Team />
 			<Events />
