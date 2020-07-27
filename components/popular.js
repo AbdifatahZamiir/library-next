77869eb49add804e7922a3854cfffbcefe67228c
abdisamad jimcale jama <albaylood1437@gmail.com>
2020-07-27 08:44:46 +0300

    added popular component

---
+++ components/popular.js
@@ -0,0 +1,355 @@
+import Carousel from "react-bootstrap/Carousel";
+import styled from "styled-components";
+
+const CarouselWrapper = styled.div``;
+
+export default function Popular() {
+	return (
+		<div className="popular_courses">
+			<div className="row justify-content-center">
+				<div className="col-lg-5">
+					<div className="main_title">
+						<h2 className="mb-3">Our Popular Courses</h2>
+						<p>
+							Replenish man have thing gathering lights yielding
+							shall you
+						</p>
+					</div>
+				</div>
+			</div>
+			<CarouselWrapper className="carousel-control-prev-icon carousel-control-next-icon">
+				<Carousel>
+					<Carousel.Item>
+						<div className="container">
+							<div className="row">
+								<div className="col-lg-4 col-md-6 col-sm-12 single_course">
+									<div className="course_head">
+										<img
+											className="img-fluid"
+											src="/images/courses/c1.jpg"
+											alt="course2"
+										/>
+									</div>
+									<div className="course_content">
+										<span className="price">$25</span>
+										<span className="tag mb-4 d-inline-block">
+											design
+										</span>
+										<h4 className="mb-3">
+											<a href="course-details.html">
+												Custom Product Design
+											</a>
+										</h4>
+										<p>
+											One make creepeth man bearing their
+											one firmament won't fowl meat over
+											sea
+										</p>
+										<div className="course_meta d-flex justify-content-lg-between align-items-lg-center flex-lg-row flex-column mt-4">
+											<div className="authr_meta">
+												<img
+													src="img/courses/author1.png"
+													alt=""
+												/>
+												<span className="d-inline-block ml-2">
+													Cameron
+												</span>
+											</div>
+											<div className="mt-lg-0 mt-3">
+												<span className="meta_info mr-4">
+													<a href="#">
+														{" "}
+														<i className="ti-user mr-2"></i>
+														25{" "}
+													</a>
+												</span>
+												<span className="meta_info">
+													<a href="#">
+														{" "}
+														<i className="ti-heart mr-2"></i>
+														35{" "}
+													</a>
+												</span>
+											</div>
+										</div>
+									</div>
+								</div>
+
+								<div className="col-lg-4 col-md-6 col-sm-12 single_course">
+									<div className="course_head">
+										<img
+											className="img-fluid"
+											src="/images/courses/c2.jpg"
+											alt="course2"
+										/>
+									</div>
+									<div className="course_content">
+										<span className="price">$25</span>
+										<span className="tag mb-4 d-inline-block">
+											design
+										</span>
+										<h4 className="mb-3">
+											<a href="course-details.html">
+												Social Media Network
+											</a>
+										</h4>
+										<p>
+											One make creepeth man bearing their
+											one firmament won't fowl meat over
+											sea
+										</p>
+										<div className="course_meta d-flex justify-content-lg-between align-items-lg-center flex-lg-row flex-column mt-4">
+											<div className="authr_meta">
+												<img
+													src="img/courses/author2.png"
+													alt=""
+												/>
+												<span className="d-inline-block ml-2">
+													Cameron
+												</span>
+											</div>
+											<div className="mt-lg-0 mt-3">
+												<span className="meta_info mr-4">
+													<a href="#">
+														{" "}
+														<i className="ti-user mr-2"></i>
+														25{" "}
+													</a>
+												</span>
+												<span className="meta_info">
+													<a href="#">
+														{" "}
+														<i className="ti-heart mr-2"></i>
+														35{" "}
+													</a>
+												</span>
+											</div>
+										</div>
+									</div>
+								</div>
+
+								<div className="col-lg-4 col-md-6 col-sm-12 single_course">
+									<div className="course_head">
+										<img
+											className="img-fluid"
+											src="/images/courses/c3.jpg"
+											alt="course3"
+										/>
+									</div>
+									<div className="course_content">
+										<span className="price">$25</span>
+										<span className="tag mb-4 d-inline-block">
+											design
+										</span>
+										<h4 className="mb-3">
+											<a href="course-details.html">
+												Computer Engineering
+											</a>
+										</h4>
+										<p>
+											One make creepeth man bearing their
+											one firmament won't fowl meat over
+											sea
+										</p>
+										<div className="course_meta d-flex justify-content-lg-between align-items-lg-center flex-lg-row flex-column mt-4">
+											<div className="authr_meta">
+												<img
+													src="img/courses/author3.png"
+													alt=""
+												/>
+												<span className="d-inline-block ml-2">
+													Cameron
+												</span>
+											</div>
+											<div className="mt-lg-0 mt-3">
+												<span className="meta_info mr-4">
+													<a href="#">
+														{" "}
+														<i className="ti-user mr-2"></i>
+														25{" "}
+													</a>
+												</span>
+												<span className="meta_info">
+													<a href="#">
+														{" "}
+														<i className="ti-heart mr-2"></i>
+														35{" "}
+													</a>
+												</span>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</Carousel.Item>
+
+					<Carousel.Item>
+						<div className="container">
+							<div className="row owl-carousel active_course">
+								<div className="col-lg-4 col-md-6 col-sm-12 single_course">
+									<div className="course_head">
+										<img
+											className="img-fluid"
+											src="/images/courses/c1.jpg"
+											alt="course2"
+										/>
+									</div>
+									<div className="course_content">
+										<span className="price">$25</span>
+										<span className="tag mb-4 d-inline-block">
+											design
+										</span>
+										<h4 className="mb-3">
+											<a href="course-details.html">
+												Custom Product Design
+											</a>
+										</h4>
+										<p>
+											One make creepeth man bearing their
+											one firmament won't fowl meat over
+											sea
+										</p>
+										<div className="course_meta d-flex justify-content-lg-between align-items-lg-center flex-lg-row flex-column mt-4">
+											<div className="authr_meta">
+												<img
+													src="img/courses/author1.png"
+													alt=""
+												/>
+												<span className="d-inline-block ml-2">
+													Cameron
+												</span>
+											</div>
+											<div className="mt-lg-0 mt-3">
+												<span className="meta_info mr-4">
+													<a href="#">
+														{" "}
+														<i className="ti-user mr-2"></i>
+														25{" "}
+													</a>
+												</span>
+												<span className="meta_info">
+													<a href="#">
+														{" "}
+														<i className="ti-heart mr-2"></i>
+														35{" "}
+													</a>
+												</span>
+											</div>
+										</div>
+									</div>
+								</div>
+
+								<div className="col-lg-4 col-md-6 col-sm-12 single_course">
+									<div className="course_head">
+										<img
+											className="img-fluid"
+											src="/images/courses/c2.jpg"
+											alt="course2"
+										/>
+									</div>
+									<div className="course_content">
+										<span className="price">$25</span>
+										<span className="tag mb-4 d-inline-block">
+											design
+										</span>
+										<h4 className="mb-3">
+											<a href="course-details.html">
+												Social Media Network
+											</a>
+										</h4>
+										<p>
+											One make creepeth man bearing their
+											one firmament won't fowl meat over
+											sea
+										</p>
+										<div className="course_meta d-flex justify-content-lg-between align-items-lg-center flex-lg-row flex-column mt-4">
+											<div className="authr_meta">
+												<img
+													src="img/courses/author2.png"
+													alt=""
+												/>
+												<span className="d-inline-block ml-2">
+													Cameron
+												</span>
+											</div>
+											<div className="mt-lg-0 mt-3">
+												<span className="meta_info mr-4">
+													<a href="#">
+														{" "}
+														<i className="ti-user mr-2"></i>
+														25{" "}
+													</a>
+												</span>
+												<span className="meta_info">
+													<a href="#">
+														{" "}
+														<i className="ti-heart mr-2"></i>
+														35{" "}
+													</a>
+												</span>
+											</div>
+										</div>
+									</div>
+								</div>
+
+								<div className="col-lg-4 col-md-6 col-sm-12 single_course">
+									<div className="course_head">
+										<img
+											className="img-fluid"
+											src="/images/courses/c3.jpg"
+											alt="course3"
+										/>
+									</div>
+									<div className="course_content">
+										<span className="price">$25</span>
+										<span className="tag mb-4 d-inline-block">
+											design
+										</span>
+										<h4 className="mb-3">
+											<a href="course-details.html">
+												Computer Engineering
+											</a>
+										</h4>
+										<p>
+											One make creepeth man bearing their
+											one firmament won't fowl meat over
+											sea
+										</p>
+										<div className="course_meta d-flex justify-content-lg-between align-items-lg-center flex-lg-row flex-column mt-4">
+											<div className="authr_meta">
+												<img
+													src="img/courses/author3.png"
+													alt=""
+												/>
+												<span className="d-inline-block ml-2">
+													Cameron
+												</span>
+											</div>
+											<div className="mt-lg-0 mt-3">
+												<span className="meta_info mr-4">
+													<a href="#">
+														{" "}
+														<i className="ti-user mr-2"></i>
+														25{" "}
+													</a>
+												</span>
+												<span className="meta_info">
+													<a href="#">
+														{" "}
+														<i className="ti-heart mr-2"></i>
+														35{" "}
+													</a>
+												</span>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</Carousel.Item>
+				</Carousel>
+			</CarouselWrapper>
+		</div>
+	);
+}
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
 import Head from "next/head";
 import Layout from "../components/layout";
 import Slider from "../components/slider";
+import Popular from "../components/popular";
 
 export default function Home() {
 	return (
 		<Layout>
 			<Slider />
+			<Popular />
 		</Layout>
 	);
 }
