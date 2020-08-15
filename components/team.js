170480b2eef2bfd5c8fd053d7a1dd58163dac424
Abdifatah <abdifatahzammiir@gmail.com>
2020-08-15 11:52:12 +0300

    add teams

--- components/team.js
+++ components/team.js
@@ -1,6 +1,6 @@
 import { FaFacebook, FaInstagram, FaTwitter, FaLinkedin } from "react-icons/fa";
 
-export default function Team() {
+export default function Team({ teamdata }) {
 	return (
 		<section className="trainer_area section_gap_top">
 			<div className="container">
@@ -16,175 +16,52 @@ export default function Team() {
 					</div>
 				</div>
 				<div className="row justify-content-center d-flex align-items-center">
-					<div className="col-lg-3 col-md-6 col-sm-12 single-trainer">
-						<div className="thumb d-flex justify-content-sm-center">
-							<img
-								className="img-fluid"
-								src="/images/trainer/t1.jpg"
-								alt="trainer1"
-							/>
-						</div>
-						<div className="meta-text text-sm-center">
-							<h4>Mated Nithan</h4>
-							<p className="designation">Sr. web designer</p>
-							<div className="mb-4">
-								<p>
-									If you are looking at blank cassettes on the
-									web, you may be very confused at the.
-								</p>
+					{teamdata.map((team) => {
+						const { name, bookImg, description, status } = team;
+						return (
+							<div
+								className="col-lg-3 col-md-6 col-sm-12 single-trainer"
+								key={name}
+							>
+								<div className="thumb d-flex justify-content-sm-center">
+									<img
+										className="img-fluid"
+										src={bookImg}
+										alt="trainer1"
+									/>
+								</div>
+								<div className="meta-text text-sm-center">
+									<h4>{name}</h4>
+									<p className="designation">{status}</p>
+									<div className="mb-4">
+										<p>{description}</p>
+									</div>
+									<div className="align-items-center justify-content-center d-flex">
+										<a href="#">
+											<i className="ti-facebook">
+												<FaFacebook />
+											</i>
+										</a>
+										<a href="#">
+											<i className="ti-twitter">
+												<FaTwitter />
+											</i>
+										</a>
+										<a href="#">
+											<i className="ti-linkedin">
+												<FaInstagram />
+											</i>
+										</a>
+										<a href="#">
+											<i className="ti-pinterest">
+												<FaLinkedin />
+											</i>
+										</a>
+									</div>
+								</div>
 							</div>
-							<div className="align-items-center justify-content-center d-flex">
-								<a href="#">
-									<i className="ti-facebook">
-										<FaFacebook />
-									</i>
-								</a>
-								<a href="#">
-									<i className="ti-twitter">
-										<FaTwitter />
-									</i>
-								</a>
-								<a href="#">
-									<i className="ti-linkedin">
-										<FaInstagram />
-									</i>
-								</a>
-								<a href="#">
-									<i className="ti-pinterest">
-										<FaLinkedin />
-									</i>
-								</a>
-							</div>
-						</div>
-					</div>
-
-					<div className="col-lg-3 col-md-6 col-sm-12 single-trainer">
-						<div className="thumb d-flex justify-content-sm-center">
-							<img
-								className="img-fluid"
-								src="/images/trainer/t2.jpg"
-								alt="trainer2"
-							/>
-						</div>
-						<div className="meta-text text-sm-center">
-							<h4>David Cameron</h4>
-							<p className="designation">Sr. web designer</p>
-							<div className="mb-4">
-								<p>
-									If you are looking at blank cassettes on the
-									web, you may be very confused at the.
-								</p>
-							</div>
-							<div className="align-items-center justify-content-center d-flex">
-								<a href="#">
-									<i className="ti-facebook">
-										<FaFacebook />
-									</i>
-								</a>
-								<a href="#">
-									<i className="ti-twitter">
-										<FaTwitter />
-									</i>
-								</a>
-								<a href="#">
-									<i className="ti-linkedin">
-										<FaInstagram />
-									</i>
-								</a>
-								<a href="#">
-									<i className="ti-pinterest">
-										<FaLinkedin />
-									</i>
-								</a>
-							</div>
-						</div>
-					</div>
-
-					<div className="col-lg-3 col-md-6 col-sm-12 single-trainer">
-						<div className="thumb d-flex justify-content-sm-center">
-							<img
-								className="img-fluid"
-								src="/images/trainer/t3.jpg"
-								alt="trainer3"
-							/>
-						</div>
-						<div className="meta-text text-sm-center">
-							<h4>Jain Redmel</h4>
-							<p className="designation">
-								Sr. Faculty Data Science
-							</p>
-							<div className="mb-4">
-								<p>
-									If you are looking at blank cassettes on the
-									web, you may be very confused at the.
-								</p>
-							</div>
-							<div className="align-items-center justify-content-center d-flex">
-								<a href="#">
-									<i className="ti-facebook">
-										<FaFacebook />
-									</i>
-								</a>
-								<a href="#">
-									<i className="ti-twitter">
-										<FaTwitter />
-									</i>
-								</a>
-								<a href="#">
-									<i className="ti-linkedin">
-										<FaInstagram />
-									</i>
-								</a>
-								<a href="#">
-									<i className="ti-pinterest">
-										<FaLinkedin />
-									</i>
-								</a>
-							</div>
-						</div>
-					</div>
-
-					<div className="col-lg-3 col-md-6 col-sm-12 single-trainer">
-						<div className="thumb d-flex justify-content-sm-center">
-							<img
-								className="img-fluid"
-								src="/images/trainer/t4.jpg"
-								alt="trainer4"
-							/>
-						</div>
-						<div className="meta-text text-sm-center">
-							<h4>Nathan Macken</h4>
-							<p className="designation">Sr. web designer</p>
-							<div className="mb-4">
-								<p>
-									If you are looking at blank cassettes on the
-									web, you may be very confused at the.
-								</p>
-							</div>
-							<div className="align-items-center justify-content-center d-flex">
-								<a href="#">
-									<i className="ti-facebook">
-										<FaFacebook />
-									</i>
-								</a>
-								<a href="#">
-									<i className="ti-twitter">
-										<FaTwitter />
-									</i>
-								</a>
-								<a href="#">
-									<i className="ti-linkedin">
-										<FaInstagram />
-									</i>
-								</a>
-								<a href="#">
-									<i className="ti-pinterest">
-										<FaLinkedin />
-									</i>
-								</a>
-							</div>
-						</div>
-					</div>
+						);
+					})}
 				</div>
 			</div>
 		</section>
